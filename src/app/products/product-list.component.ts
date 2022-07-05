e6d429a7b19e0901bdcd414d5169e244159e9b57
Ionut <ciobanuionut99@gmail.com>
2022-07-05 19:51:16 +0300

    Finished Angular Tutorial

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,79 +1,71 @@
-import { Component, OnInit } from "@angular/core";
-import { IProduct } from './product';
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {IProduct} from './product';
+import {ProductService} from "./product.service";
+import {Subscription} from "rxjs";
 
 
 @Component({
-selector:'pm-products',
-templateUrl:'./product-list.component.html',
-styleUrls:['./product-list.component.css']
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
-
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
 
   private _listFilter: string = '';
 
-  get listFilter(): string{
-  return this._listFilter;
+  get listFilter(): string {
+    return this._listFilter;
   }
 
   set listFilter(value: string) {
-  this._listFilter = value;
-  console.log('In setter:', value);
-  this.filteredProducts = this.performFilter(value);
+    this._listFilter = value;
+    console.log('In setter:', value);
+    this.filteredProducts = this.performFilter(value);
   }
 
   filteredProducts: IProduct[] = [];
 
-  products: IProduct[] = [
-  {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    },
-    {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2021",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "assets/images/saw.png"
-      }];
+  products: IProduct[] = [];
 
 
-    ngOnInit(): void {
-    this.listFilter='cart';
-    }
+  constructor(private productService: ProductService) {
+  }
 
-      toggleImage(): void {
-      this.showImage = !this.showImage;
-      }
 
-      performFilter(filterBy: string): IProduct[]{
-      filterBy = filterBy.toLocaleLowerCase();
-      return this.products.filter((product: IProduct) =>
+
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
       product.productName.toLocaleLowerCase().includes(filterBy));
-      }
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 
 }
